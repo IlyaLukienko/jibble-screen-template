9c2c990df54bbc76906df372197285662b476da6
GitHub Action <action@github.com>
2021-01-04 16:56:19 +0000

    Template cleanup

--- src/main/kotlin/com/github/ilyalukienko/jibblescreentemplate/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/ilyalukienko/jibblescreentemplate/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.ilyalukienko.jibblescreentemplate.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.ilyalukienko.jibblescreentemplate.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
---
+++ src/main/kotlin/com/github/ilyalukienko/jibblescreentemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.ilyalukienko.jibblescreentemplate.services
+
+import com.github.ilyalukienko.jibblescreentemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
